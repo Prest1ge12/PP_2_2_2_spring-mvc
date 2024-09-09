24a81b7de1e02137bc7a06977a92b4e0d7d82a81
Sergey Angelov <mrangel@yandex.ru>
2024-09-09 19:46:40 +0300

    Исправленное задание

--- src/main/java/web/DAO/CarDaoImp.java
+++ src/main/java/web/DAO/CarDaoImp.java
@@ -16,7 +16,13 @@ public class CarDaoImp implements CarDao {
         carList.add(new Car(2002, 14, "Audi"));
         carList.add(new Car(2010, 15, "Bmw"));
         carList.add(new Car(2020, 16, "Mercedes"));
-        carList.add(new Car(2022, 17, "SomeCar"));
+
+        // По 5 пункту не сработает при добавлении 6-го авто не понял.
+        // При добавлении здесь авто и перезапуске томкат сервера проверил, вроде работает по заданию
+
+        carList.add(new Car(2022, 17, "SomeCar1"));
+        carList.add(new Car(2012, 19, "SomeCar2"));
+        carList.add(new Car(2032, 15, "SomeCar3"));
         return carList;
     }
 }
